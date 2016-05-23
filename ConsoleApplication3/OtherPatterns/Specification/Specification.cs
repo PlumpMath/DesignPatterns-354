ee7dce7dc012e80626ef6b76ff03f2f219254bbe
korshunovpavel <pavel222222@tut.by>
2016-05-23 22:51:23 +0300

    Specification Updated. Added Lazy initialization

--- ConsoleApplication3/OtherPatterns/Specification/Specification.cs
+++ ConsoleApplication3/OtherPatterns/Specification/Specification.cs
@@ -6,18 +6,27 @@ namespace DesignPatterns.OtherPatterns.Specification
     public abstract class Specification<T> : ISpecification<T>
     {
         public Expression<Func<T, bool>> Predicate { get; private set; }
+        private readonly Lazy<Func<T, bool>> _lazyCompiledPredicate;
+
+        private Func<T, bool> CompiledPredicate
+        {
+            get
+            {
+                return _lazyCompiledPredicate.Value;
+            }
+        }
 
 
         protected Specification(Expression<Func<T, bool>> predicate)
         {
             Predicate = predicate;
+            _lazyCompiledPredicate = new Lazy<Func<T, bool>>(predicate.Compile);
         }
 
 
         public bool IsSatisfiedBy(T o)
         {
-            var func = Predicate.Compile();
-            return func(o);
+            return CompiledPredicate(o);
         }
 
         public ISpecification<T> And(ISpecification<T> specification)
